7cd63d451a8a8f9e338af01577f4a72d31c0fb77
jerrr36 <58893325+jerrr36@users.noreply.github.com>
2020-10-13 18:02:20 -0700

    Updated and works with hivemq + esp8266

--- mqtt/mqtt.go
+++ mqtt/mqtt.go
@@ -3,27 +3,26 @@ package main
 import(
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 	"fmt"
-	"errors"
+	_ "errors"
 	"time"
 	"os"
 )
 
-qosErr := errors.New("QOS needs to be from 0 to 2")
+
 var flag bool = false
 
 var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
 	topic := msg.Topic()
 	payload := msg.Payload()
-	if strings.Compare(string(payload), "\n") > 0 {
-		fmt.Printf("Topic: %s\n", topic)
-		fmt.Printf("MSG: %s\n", payload)
-	}
+	
+	fmt.Printf("Topic: %s\n", topic)
+	fmt.Printf("MSG: %s\n", payload)
 }
 
 func main() {
-
+	//qosErr := errors.New("QOS needs to be from 0 to 2")
 	//Options for the MQTT connection
-	opts := MQTT.NewClientOptions().AddBroker("tcp://mqtt.eclipse.org:1883")
+	opts := MQTT.NewClientOptions().AddBroker("tcp://broker.hivemq.com:1883")
 	opts.SetClientID("Device-sub")
 	opts.SetDefaultPublishHandler(f)
 
@@ -32,11 +31,17 @@ func main() {
 	if token := c.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
+	if token := c.Subscribe("esp/test",0,nil); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
 
-
+	for {
+		time.Sleep(time.Second * 1)
+	}
 }
 
-//Func to publish 
+/*Func to publish 
 func Publish(topic string, msg string, qos uint16, c Client) error {
 	if qos > 2 {
 		return qosError
@@ -61,3 +66,4 @@ func Subscribe(topic string, msg string, qos uint16, c Client) error {
 
 	return nil
 }
+*/
